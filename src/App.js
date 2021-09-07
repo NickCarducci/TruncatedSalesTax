080e64a8b91c7022936306153d416111f218e552
Nick <nmcarducci@gmail.com>
2021-09-07 18:44:11 -0400

    ok

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import TwitterTweetEmbed from "./TwitterTweetEmbed";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "arial, sans serif" }}>
+      <h1>Truncated Sales Tax / Millennial Income Deficit</h1>
+      <h2>3%ile average-indexed-purchases, about $60 max at $2k</h2>
+      My mom said, “good thing you aren’t allowed to get a gun,” after me saying
+      I am anti-government and urging her to take back her lie that I pushed her
+      towards my incarceration and slander as a bipolar schizophrenic, while she
+      lives in this house that is of 44x third-party-beneficiary usurped. 1
+      Peter 2:10 , 1 John 2:22 Riverview Psycologist said, "this is Hell, yes,"
+      when I asked. Women, right?
+      <TwitterTweetEmbed
+        key="1434278985589006344"
+        tweetId="1434278985589006344"
+      />
+      <img
+        style={{ width: "100%", maxWidth: "500px" }}
+        alt="old people: @truncsalestax, froth.app"
+        src="https://www.dl.dropboxusercontent.com/s/8qbefb3qow5e27g/Screen%20Shot%202021-09-07%20at%206.19.18%20PM.png?dl=0"
+      />
+      <br />
+      employmentByCohort: "he goes beyond the Science"
+      <iframe
+        style={{ width: "100%", maxWidth: "800px", height: "500px" }}
+        src="https://vaults.biz/#:~:text=employmentByCohort"
+        title="vaults employmentByCohort"
+      />
+      <iframe
+        src="https://www.linkedin.com/embed/feed/update/urn:li:share:6841120482733916160"
+        height="524px"
+        width="504px"
+        frameborder="0px"
+        allowfullscreen=""
+        title="https://www.linkedin.com/posts/nickcarducci_unemploymentbenefits-activity-6841120483153330176-xhhz"
+      ></iframe>
+    </div>
+  );
+}
