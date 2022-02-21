63fd2a86b4e42009a534a2ebf2de45af5b541dae
Nick Carducci <nmcarducci@gmail.com>
2022-02-21 10:06:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2947,7 +2947,11 @@ export default class App extends React.Component {
         You CANNOT compete with consumers, trust-build, BECAUSE FOR READON THAT
         -GDP/p tech advancement of laborless-demand (when price-controls
         suffocates quality supply chain). Car Sheild is an investment bank
-        horizontal 1/12 (industry-)trust, 10/12 jury unbiased
+        horizontal 1/12 (industry-)trust, 10/12 jury unbiased.
+        <br />
+        <br />
+        leisure (scientific contribution) is productive, GDP/p is
+        counterproductive
       </div>
     );
   }
